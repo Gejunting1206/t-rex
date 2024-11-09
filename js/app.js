bf85e1d5f8e3eaaf818e3adf0ed933e358b3ebac
Gejunting1206 <2868008@qq.com>
2024-11-09 20:53:54 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,8 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
    let gravity = 0.9
    let isJumping = false
    let isGameOver = false
+   function again() {
+      window.location.assign("../index.html")
+   }
    function control(e) {
-      if (e.code === "Space"|| e.code === "ArrowUp"|| e.code === "w") {
+      if (e.code === "R" || e.code === "r"){
+        again()
+      }else {
         if (!isJumping) {
          jump()
         }
@@ -46,12 +51,18 @@ document.addEventListener('DOMContentLoaded', () => {
 			e.preventDefault();
    })
 
+   function mouse_jump() {
+		if(!isJumping) {
+		    jump()
+		}
+   }
+
    document.addEventListener('keydown', control)
-   document.addEventListener('mousedown', jump)
+   document.addEventListener('mousedown', mouse_jump)
  
    function generateObstacles() {
     if (!isGameOver) {
-       let randomTime = Math.random() * 3000 + 80
+       let randomTime = Math.random() * 500 + 600
     
     let obstaclePosition = 1000 
       const obstacle = document.createElement('div')
@@ -62,7 +73,12 @@ document.addEventListener('DOMContentLoaded', () => {
       let timerId = setInterval(function () {
         if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60 ) {
           clearInterval(timerId)
-          alert.innerHTML = '游戏结束！新纪录：' + fen.toFixed() + '分'
+          if (fen > window.localStorage.getItem('bestScore')) {
+            alert.innerHTML = '游戏结束！新纪录：' + fen.toFixed() + '分'
+            window.localStorage.setItem('bestScore', fen)
+          }else {
+            alert.innerHTML = '游戏结束！分数：' + fen.toFixed() + '分'
+          }
           isGameOver = true
           // remove all children
           while (grid.firstChild) {
@@ -70,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
           } 
         }
         obstaclePosition -= 10
-        fen += 0.02
+        fen += 0.008
         if(isGameOver === false){
             alert.innerHTML = '分数：' + fen.toFixed() + '分'
         }
